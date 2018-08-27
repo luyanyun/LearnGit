b3524c155fd6e9a25c5220f0071ab9b35725ce68
luyan <luyanyun@163.com>
2018-08-27 20:06:47 +0800

    新篇章

--- src/java/Teacher.java
+++ src/java/Teacher.java
@@ -1,9 +1,6 @@
 
 public class Teacher {
     public static void main(String[] args) {
-        System.out.println("你是谁");
-        System.out.println("什么鬼");
-        System.out.println("让我想一下为什么被驳回");
-        System.out.println("第二版本");
+        System.out.println("来到新的篇章");
     }
 }
