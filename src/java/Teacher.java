76ec7c46d591800d941e6cfe51eb9d1bc6c01d3a
luyan <luyanyun@163.com>
2018-08-27 20:33:27 +0800

    为何

--- src/java/Teacher.java
+++ src/java/Teacher.java
@@ -3,5 +3,7 @@ public class Teacher {
     public static void main(String[] args) {
         System.out.println("来到新的篇章");
         System.out.println("我的分支上传了没有");
+        System.out.println("为何上传不了分支"
+        );
     }
 }
