5bf77a29a62cefa25a5c56b402fb4d5a10e3e8b4
luyan <luyanyun@163.com>
2018-08-27 19:33:46 +0800

    第二次

--- src/java/Teacher.java
+++ src/java/Teacher.java
@@ -1,10 +1,9 @@
-/**
- * Created by Administrator on 2018/8/27 0027.
- */
+
 public class Teacher {
     public static void main(String[] args) {
         System.out.println("你是谁");
         System.out.println("什么鬼");
         System.out.println("让我想一下为什么被驳回");
+        System.out.println("第二版本");
     }
 }
