ae22aa05f0a591f5f0553d7abc03f899d306c210
luyan <luyanyun@163.com>
2018-08-27 19:22:12 +0800

    第一次

---
+++ src/java/Teacher.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2018/8/27 0027.
+ */
+public class Teacher {
+    public static void main(String[] args) {
+        System.out.println("你是谁");
+        System.out.println("什么鬼");
+
+    }
+}
